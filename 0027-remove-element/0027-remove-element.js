61a4b695c0a7b3150be9379cbbc36b7156e8f4ea
Preetam Chinde <75744062+prem1601@users.noreply.github.com>
2026-06-13 16:56:39 +0530

    Time: 0 ms (100%), Space: 52.5 MB (96.09%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -5,12 +5,12 @@
  */
 var removeElement = function(nums, val) {
     let k = 0;
-    const tempMap = {}
+    // const tempMap = {};
 
     for(let i = 0; i < nums.length; i++) {
        if(nums[i] !== val) {
-            nums[k] = nums[i]
-            k++
+            nums[k] = nums[i];
+            k++;
             // if(!isNaN(tempMap[nums[i]])) tempMap[nums[i]]++;
             // else tempMap[nums[i]] = 1
         }
