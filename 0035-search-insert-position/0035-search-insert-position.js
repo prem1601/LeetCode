ef907c8f1aaf0081b0794a0b43cefac511cb3b77
Preetam Chinde <75744062+prem1601@users.noreply.github.com>
2026-06-15 18:46:47 +0530

    Time: 56 ms (1.41%), Space: 55.3 MB (5.86%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -16,21 +16,36 @@ var searchInsert = function(nums, target) {
 
     // return index;
 
+    // let min = 0;
+    // let max = nums.length - 1;
 
-    let index = 0;
+    // if(target > nums[max]) return max + 1;
+    // if(target < nums[min]) return min;
+
+    // while(min < max){
+    //     if(target === nums[min]) return min;
+    //     else if(target === nums[max]) return max;
+
+    //     if(target > nums[min]) min++;
+    //     else max--;
+    // }
+
+    // return max;
+
     let min = 0;
     let max = nums.length - 1;
 
-    if(target > nums[max]) return max + 1;
-    if(target < nums[min]) return min;
+    if (target > nums[max]) return max + 1;
+    if (target < nums[min]) return min;
 
-    while(min < max){
-        if(target === nums[min]) return min;
-        else if(target === nums[max]) return max;
+    while (min <= max) {
+        const half = Math.floor((min + max) / 2);
 
-        if(target > nums[min]) min++;
-        else max--;
+        if (target === nums[half]) return half;
+        else if (target > nums[half]) min = half + 1;
+        else max = half - 1;
+
     }
 
-    return max;
+    return min;
 };
